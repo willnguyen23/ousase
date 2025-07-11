c06143beca7c86c449a1619dc2b04fab3f56ca1a
wvnguyen23 <wvnguyen23@gmail.com>
2025-07-10 22:06:57 -0500

    one more

--- src/Components/Exec.tsx
+++ src/Components/Exec.tsx
@@ -13,112 +13,112 @@ const memberImages: MemberImages[] = [
         id: "daniel",
         num: 1,
         alt: "daniel park - president",
-        src: "Images/daniel.png",
+        src: "./Images/daniel.png",
         info: ["Daniel Park", "President", "Chemical Engineering", "Junior"]
     }, 
     {
         id: "ian",
         num: 2,
         alt: "ian dang - vpx",
-        src: "Images/ian.png",
+        src: "./Images/ian.png",
         info: ["Ian Dang", "Vice President External", "Computer Science", "Junior"]
     },
     {
         id: "donald",
         num: 3,
         alt: "donald nguyen - vpi",
-        src: "Images/donald.png",
+        src: "./Images/donald.png",
         info: ["Donald Nguyen", "Vice President Internal", "Management Information Systems", "Senior"]
     }, 
     {
         id: "sarah",
         num: 4,
         alt: "sarah kim - secretary",
-        src: "Images/sarah.png",
+        src: "./Images/sarah.png",
         info: ["Sarah Kim", "Secretary", "Chemical Biosciences", "Junior"]
     }, 
     {
         id: "josh",
         num: 5,
         alt: "josh tu - finance",
-        src: "Images/blank.png",
+        src: "./Images/blank.png",
         info: ["Joshua Tu", "Director of Finance", "Computer Engineering", "Junior"]
     }, 
     {
         id: "janes",
         num: 6,
         alt: "janes le - community",
-        src: "Images/janes.png",
+        src: "./Images/janes.png",
         info: ["Janes Le", "Community Chair", "Computer Engineering", "Junior"]
     },
     {
         id: "dylan",
         num: 7,
         alt: "dylan quan - community",
-        src: "Images/blank.png",
+        src: "./Images/blank.png",
         info: ["Dylan Quan", "Community Chair", "Industrial and Systems Engineering", "Senior"]
     }, 
     {
         id: "anson",
         num: 8,
         alt: "anson tang - marketing",
-        src: "Images/anson.png",
+        src: "./Images/anson.png",
         info: ["Anson Tang", "Marketing Director", "Mechanical Engineering", "Junior"]
     }, 
     {
         id: "don",
         num: 9,
         alt: "don dao - graphics",
-        src: "Images/blank.png",
+        src: "./Images/blank.png",
         info: ["Don Dao", "Graphic Designer", "Information Science and Technology", "Senior"]
     }, 
     {
         id: "angela",
         num: 10,
         alt: "angela vu - creative",
-        src: "Images/blank.png",
+        src: "./Images/blank.png",
         info: ["Angela Vu", "Creative Chair", "Computer Engineering", "Sophomore"]
     },
     {
         id: "melissa",
         num: 11,
         alt: "melissa ng - marketing asst",
-        src: "Images/melissa.png",
+        src: "./Images/melissa.png",
         info: ["Melissa Ng", "Marketing Assistant", "Computer Science", "Senior"]
     }, 
     {
         id: "bea",
         num: 12,
         alt: "bea nazareno - intern coor",
-        src: "Images/blank.png",
+        src: "./Images/blank.png",
         info: ["Bea Nazareno", "Intern Coordinator", "Biomedical Engineering", "Senior"]
     }, 
     {
         id: "will",
         num: 13,
         alt: "william nguyen - intern coor.",
-        src: "Images/blank.png",
+        src: "./Images/blank.png",
         info: ["William Nguyen", "Intern Coordinator", "Computer Engineering", "Junior"]
     }, 
     {
         id: "jet",
         num: 14,
         alt: "jet flener - event coor.",
-        src: "Images/blank.png",
+        src: "./Images/blank.png",
         info: ["Jet Flener", "Event Coordinator", "Computer Engineering", "Junior"]
     },
     {
         id: "carina",
         num: 15,
         alt: "carina chen - event coor.",
-        src: "Images/blank.png",
+        src: "./Images/blank.png",
         info: ["Carina Chen", "Event Coordinator", "Biology (Pre-Dent)", "Sophomore"]
     }, 
     {
         id: "grace",
         num: 16,
         alt: "grace chaing - science chair",
-        src: "Images/blank.png",
+        src: "./Images/blank.png",
         info: ["Grace Chiang", "Science Chair", "Pre-Nursing", "Sophomore"]
     }, 
 ]
