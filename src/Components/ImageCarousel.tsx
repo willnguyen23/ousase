676040471c7eb19220e82b94228c1cee03fcf29e
wvnguyen23 <wvnguyen23@gmail.com>
2025-07-10 23:42:21 -0500

    f

--- src/Components/ImageCarousel.tsx
+++ src/Components/ImageCarousel.tsx
@@ -6,27 +6,27 @@ export default function Carousel() {
                     <div>
                         <div className="carousel">
                             <article>
-                                <img src="/ousase/assets/Images/finalGBM2025.jpg" alt="final-gbm" />
+                                <img src="/Images/finalGBM2025.jpg" alt="final-gbm" />
                             </article>
 
                             <article>
-                                <img src="/ousase/assets/Images/gbmFour.jpg" alt="sase-gbm4" />
+                                <img src="/Images/gbmFour.jpg" alt="sase-gbm4" />
                             </article>
 
                             <article>
-                                <img src="/ousase/assets/Images/saseNatcon2025.jpg" alt="sase-natcon25" />
+                                <img src="/Images/saseNatcon2025.jpg" alt="sase-natcon25" />
                             </article>
 
                             <article>
-                                <img src="/ousase/assets/Images/saseVolunteer.jpg" alt="saseunteer" />
+                                <img src="/Images/saseVolunteer.jpg" alt="saseunteer" />
                             </article>
 
                             <article>
-                                <img src="/ousase/assets/Images/saseLNY.jpg" alt="sase-lny" />
+                                <img src="/Images/saseLNY.jpg" alt="sase-lny" />
                             </article>
 
                             <article>
-                                <img src="/ousase/assets/Images/saseHalloween.jpg" alt="saseoween" />
+                                <img src="/Images/saseHalloween.jpg" alt="saseoween" />
                             </article>
                         </div>
                     </div>
