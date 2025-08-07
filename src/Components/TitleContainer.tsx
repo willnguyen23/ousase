b46e4ec7e4552cce75c550abacfecd82566d0a00
iandang312 <iandang312@gmail.com>
2025-08-07 11:52:15 -0500

    Commented out buttons from title container

--- src/Components/TitleContainer.tsx
+++ src/Components/TitleContainer.tsx
@@ -10,11 +10,11 @@ export default function TitleContainer() {
                     <h1>Society of Asian Scientists and Engineers</h1>
                     <h3>University of Oklahoma</h3>
                 </div>
-                <div className="intro-navbar">
+                {/* <div className="intro-navbar">
                     <Link to="/about" className="intro-navbar-button"><b>About Us</b></Link>
                     <Link to="/sponsor" className="intro-navbar-button"><b>Sponsor Info</b></Link>
                     <Link to="/faq" className="intro-navbar-button"><b>FAQ</b></Link>
-                </div>
+                </div> */}
             </div>
         </>
     )
